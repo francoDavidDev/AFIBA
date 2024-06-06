ed1ffe08d12f63dd15adf8662e01d6aed892af23
Franco David Gomez <francodavid_dev@outlook.com>
2024-06-06 16:18:03 -0300

    gif video

--- src/assets/components/LoadingScreen.jsx
+++ src/assets/components/LoadingScreen.jsx
@@ -1,38 +1,21 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      delay: 0.5,
-      duration: 1,
-    },
-  },
-};
-
-const textVariants = {
-  hidden: { y: -50, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-    transition: { duration: 1.5, ease: "easeInOut" },
-  },
-};
+import video from '../videos/afibaGif.mp4';
 
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
       <motion.div
         className="loader-container"
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.5 }}
       >
-        <motion.h1 className="loader-text" variants={textVariants}>
-          AFIBA
-        </motion.h1>
+        <video className="loader-video" autoPlay muted>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </motion.div>
       <style jsx>{`
         .loading-screen {
@@ -40,16 +23,17 @@ const LoadingScreen = () => {
           justify-content: center;
           align-items: center;
           height: 100vh;
-          background-color: #4caf50;
-          color: #0073e6;
-          font-family: Arial, sans-serif;
+          width: 100vw;
+          background-color: #000;
+          overflow: hidden;
         }
         .loader-container {
           text-align: center;
         }
-        .loader-text {
-          font-size: 3rem;
-          font-weight: bold;
+        .loader-video {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
         }
       `}</style>
     </div>
